c35f294d3f63af48eae51560fb45acccb84aa482
marcos salinas <marcossalinasf@gmail.com>
2019-10-28 14:18:35 +0100

    pub

---
+++ src/app/core/layout/main/main.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styles: []
+})
+export class MainComponent implements OnInit {
+
+  title = 'curso angular';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
