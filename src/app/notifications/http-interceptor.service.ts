33a293a847eaba5a144d94e823d5830c9e8fbecd
Marcos (curro) <marcossalinasf@gmail.com>
2019-11-12 19:34:19 +0100

    un par de cambios

--- src/app/notifications/http-interceptor.service.ts
+++ src/app/notifications/http-interceptor.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap, map, catchError } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { NotificationsStoreService } from './notifications-store.service';
 import { DateTimeService } from '../core/date-time.service';
 
@@ -15,9 +15,7 @@ export class HttpInterceptorService implements HttpInterceptor {
   constructor(private notificationsStore: NotificationsStoreService, private dateTimeService: DateTimeService) { }
 
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // return next.handle(req).pipe(tap((event: HttpEvent<any>) => { this.notificationsStore.dispatch(event.url + ' ' + event.status); } ));
     return next.handle(req).pipe(tap(this.dispatch.bind(this) ));
-    // return next.handle(req).pipe(tap((event: HttpEvent<any>) => { console.log(event); } ));
   }
 
   private dispatch(message: HttpEvent<any>) {
